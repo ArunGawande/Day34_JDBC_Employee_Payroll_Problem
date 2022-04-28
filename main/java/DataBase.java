c97de81e205a862281b886708f697cf1ca2cdbaf
Arun <arungawande1995@gmail.com>
2022-04-28 23:29:42 +0530

    Added: UC1 to connect a Database

---
+++ main/java/DataBase.java
@@ -0,0 +1,38 @@
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class DataBase
+{
+    public static void main(String[] args) {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/payrollService?user=root";
+        String userName = "root";
+        String password = "Arun@12345";
+        Connection connection;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded");
+        } catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        listDrivers();
+        try {
+            System.out.println("Connecting to database: " + jdbcUrl);
+            connection = DriverManager.getConnection(jdbcUrl, userName, password);
+            System.out.println("Connection is Successful" + connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println(" " + driverClass.getClass().getName());
+        }
+    }
+}
